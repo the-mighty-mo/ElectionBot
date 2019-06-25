eea9b10eb505772fa48fbf0bf244c04ad41064e7
Benjamin Hall <hallb1016@gmail.com>
2019-06-25 02:14:25 -0400

    Update SecurityInfo.cs
    Ready for Azure DevOps

--- SecurityInfo.cs
+++ SecurityInfo.cs
@@ -6,7 +6,7 @@ namespace ElectionBot
     {
         public const string token = "Insert Bot Token Here";
         public const string botID = "Insert Bot User Discriminator Here";
-        public const ulong botClient = Insert Bot Client ID Here;
+        public const ulong botClient = 0; // Insert Bot Client ID Here
         public const string botName = "Insert Bot Name here";
         public const string botVersion = "Insert Bot Version here";
         public static readonly Color botColor = new Color(0, 90, 20);
